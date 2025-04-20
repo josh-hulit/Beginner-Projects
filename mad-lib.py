f676d15f8ea20f888af6b3cf994ae5d6e45a0b7e
josh-hulit <joshhulit@gmail.com>
2025-04-20 08:31:37 -0400

    Add files via upload

---
+++ mad-lib.py
@@ -0,0 +1,19 @@
+# Goat Mad Lib
+
+
+noun = input("Enter a noun: ")
+name = input("Enter a name: ")
+plural_noun = input("Enter a plural noun: ")
+verb_ing = input("Enter a verb ending in 'ing': ")
+city = input("Enter a city: ")
+adj = input("Enter an adjective: ")
+num = input("Enter a number: ")
+verb = input("Enter a verb: ")
+
+
+print("On a homestead in the city of " + city + " there lived" "\n"
+      "a goat named " + name + ". He was a good goat who enjoyed " + verb_ing + "\n"
+      "with his friends in the " + noun + "." "\n"
+      "There were " + num + " other goats that were very " + adj + "." "\n"
+      "But " + name + " was always " + verb_ing + " and that was good." "\n"
+      "How many other " + plural_noun + " could be this happy?")
