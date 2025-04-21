eaeb54d4c8d8a87581e414c75899392e20db0e9c
Josh <joshhulit@gmail.com>
2025-04-20 22:44:55 -0400

    Add files via upload

---
+++ guess_the_number.py
@@ -0,0 +1,40 @@
+
+# Number Guessing Game
+
+import random
+
+lowest_number = 1
+highest_number = 100
+
+answer = random.randint(lowest_number, highest_number)
+
+guesses = 0
+is_running = True
+
+print("Welcome to the Number Guessing Game")
+print(f"Please select a number between {lowest_number} and {highest_number}: ")
+
+while is_running:
+    guess = input("Enter your guess: ")
+
+    if guess.isdigit():
+        guess = int(guess)
+        guesses += 1
+
+    else:
+        print("Invalid Response, please try again: ")
+
+    if guess < lowest_number or guess > highest_number:
+        print("Invalid Response, please try again: ")
+
+    elif guess < answer:
+        print("Too low, please try again: ")
+
+    elif guess > answer:
+        print("Too high, please try again: ")
+
+    else:
+        print("Congratulations")
+        print(f"You got the answer in {guesses} tries!")
+        print("End")
+        is_running = False
